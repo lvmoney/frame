69769ee0a83982e7fc49cab219f53bbeb955a870
lvmoney <xmangl1990728>
2020-07-27 16:54:53 +0800

    重复提交限制测试通过

--- frame-common/src/main/java/com/lvmoney/common/util/ApplicationBeanUtil.java
+++ frame-common/src/main/java/com/lvmoney/common/util/ApplicationBeanUtil.java
@@ -11,7 +11,11 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.lang.annotation.Annotation;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,13 +31,23 @@ public class ApplicationBeanUtil implements ApplicationContextAware {
     /**
      * 上下文
      */
-    private static ApplicationContext applicationContext;
+    private static ApplicationContext applicationContext = null;
+
+    /**
+     * 获取静态变量中的ApplicationContext.
+     */
+    public static ApplicationContext getApplicationContext() {
+        return ApplicationBeanUtil.applicationContext;
+    }
 
     /**
      * 默认构造方法，注入上下文
      *
-     * @param applicationContext
-     * @return
+     * @param applicationContext:
+     * @throws
+     * @return: void
+     * @author: lvmoney /XXXXXX科技有限公司
+     * @date: 2020/6/16 14:36
      */
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
@@ -43,15 +57,17 @@ public class ApplicationBeanUtil implements ApplicationContextAware {
     /**
      * 获取 bean
      *
-     * @param clazz
-     * @return
+     * @param clazz:
+     * @throws
+     * @return: T
+     * @author: lvmoney /XXXXXX科技有限公司
+     * @date: 2020/6/16 14:36
      */
-
     public static <T> T getBean(Class<T> clazz) {
         T obj;
         try {
             //从上下文获取 bean
-            obj = applicationContext.getBean(clazz);
+            obj = getApplicationContext().getBean(clazz);
         } catch (Exception e) {
             obj = null;
         }
@@ -62,15 +78,18 @@ public class ApplicationBeanUtil implements ApplicationContextAware {
     /**
      * 获取 bean 的类型
      *
-     * @param clazz
-     * @return
+     * @param clazz:
+     * @throws
+     * @return: java.util.List<T>
+     * @author: lvmoney /XXXXXX科技有限公司
+     * @date: 2020/6/16 14:36
      */
     public static <T> List<T> getListBeansOfType(Class<T> clazz) {
         //声明一个结果
         Map<String, T> map;
         try {
             //获取类型
-            map = applicationContext.getBeansOfType(clazz);
+            map = getApplicationContext().getBeansOfType(clazz);
         } catch (Exception e) {
             map = null;
         }
@@ -78,19 +97,21 @@ public class ApplicationBeanUtil implements ApplicationContextAware {
         return map == null ? null : new ArrayList<>(map.values());
     }
 
-
     /**
      * 获取 bean 的类型
      *
-     * @param clazz
-     * @return
+     * @param clazz:
+     * @throws
+     * @return: java.util.Map<java.lang.String, T>
+     * @author: lvmoney /XXXXXX科技有限公司
+     * @date: 2020/6/16 14:36
      */
     public static <T> Map<String, T> getMapBeansOfType(Class<T> clazz) {
         //声明一个结果
         Map<String, T> map;
         try {
             //获取类型
-            map = applicationContext.getBeansOfType(clazz);
+            map = getApplicationContext().getBeansOfType(clazz);
         } catch (Exception e) {
             map = null;
         }
@@ -102,17 +123,58 @@ public class ApplicationBeanUtil implements ApplicationContextAware {
     /**
      * 获取所有被注解的 bean
      *
-     * @param anno
-     * @return
+     * @param anno:
+     * @throws
+     * @return: java.util.Map<java.lang.String, java.lang.Object>
+     * @author: lvmoney /XXXXXX科技有限公司
+     * @date: 2020/6/16 14:35
      */
     public static Map<String, Object> getBeansWithAnnotation(Class<? extends Annotation> anno) {
         Map<String, Object> map;
         try {
             //获取注解的 bean
-            map = applicationContext.getBeansWithAnnotation(anno);
+            map = getApplicationContext().getBeansWithAnnotation(anno);
         } catch (Exception e) {
             map = null;
         }
         return map;
+    }
+
+    /**
+     * 获取当前用户的token
+     *
+     * @throws
+     * @return: java.lang.String
+     * @author: lvmoney /XXXXXX科技有限公司
+     * @date: 2020/6/2 16:50
+     */
+    public static String getToken() {
+        HttpServletRequest request = getHttpServletRequest();
+        String token = request.getHeader("token");
+        return token;
+    }
+
+    /**
+     * 获得当前的HttpServletRequest
+     *
+     * @throws
+     * @return: javax.servlet.http.HttpServletRequest
+     * @author: lvmoney /XXXXXX科技有限公司
+     * @date: 2020/6/2 17:21
+     */
+    public static HttpServletRequest getHttpServletRequest() {
+        return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+    }
+
+    /**
+     * 获得当前的HttpServletResponse
+     *
+     * @throws
+     * @return: javax.servlet.http.HttpServletResponse
+     * @author: lvmoney /XXXXXX科技有限公司
+     * @date: 2020/6/2 17:27
+     */
+    public static HttpServletResponse getHttpServletResponse() {
+        return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getResponse();
     }
 }
